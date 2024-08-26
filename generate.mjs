17901d877510c53d681e8b5c618da8f47abecc30
madaley1 <mor2daley@gmail.com>
2024-08-26 15:17:14 -0700

    add home generation

--- generate.mjs
+++ generate.mjs
@@ -89,7 +89,8 @@ const generatePostsPage = (postPaths) => {
 
     const postsFilePath = join(__dirname, "/posts/index.html")
     const fd = openSync(postsFilePath, 'w+')
-    const currentFile = readFileSync(fd);
+    const currentFile = readFileSync(fd, { encoding: 'utf8', flag: 'r' });
+    console.log(currentFile === fileContents)
     if(currentFile === fileContents) {
       close(fd);
     } else{
@@ -102,13 +103,15 @@ const generatePostsPage = (postPaths) => {
 const generatePostFiles = (postPaths) => {
   for(const postPath of postPaths){
     const mdFile = postPath + ".md";
+    const splitPath = postPath.split('/')
+    const postTitle = splitPath[splitPath.length -1]
     const postHTML = translateMdToHTML(mdFile);
     const beginningOfFile = `
       <!DOCTYPE html>
       <html lang="en">
         <head>
           <meta charset="UTF-8" />
-          <title>Nova Blog</title>
+          <title>${postTitle} | Nova Blog</title>
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="description" content="" />
           <link rel="icon" href="favicon.png">
@@ -129,7 +132,7 @@ const generatePostFiles = (postPaths) => {
     const fileContents = beginningOfFile + bodyOfFile + endOfFile;
 
     const fd = openSync(postPath+".html", 'w+')
-    const currentFile = readFileSync(fd);
+    const currentFile = readFileSync(fd, { encoding: 'utf8', flag: 'r' });
     if(currentFile === fileContents) {
       close(fd);
     } else{
@@ -137,6 +140,44 @@ const generatePostFiles = (postPaths) => {
       close(fd);
     }
   }
+}
+
+const generateHomePage = (postPaths) => {
+  let linkList = ''
+  for(let i = postPaths.length; i - 1 > -1 && i > postPaths.length - 5; i--){
+    const postPath = postPaths[i - 1];
+
+    console.log(i, postPath)
+
+    const splitPostPath = postPath.split('/');
+    
+    const postName = splitPostPath[splitPostPath.length - 1];
+    const postDate = splitPostPath[splitPostPath.length - 2];
+    linkList += `
+      <div class="postLink">
+        <a href="/${relative('.', postPath)}.html">${capitalizeFirstLetter(postName)} | ${postDate}</a>
+      </div>
+    `
+  }
+
+  // const fileContents = beginningOfFile + bodyOfFile + endOfFile;
+
+  const homeFilePath = join(__dirname, "/index.html")
+  const currentFile = readFileSync(homeFilePath, { encoding: 'utf8', flag: 'r' });
+  console.log(typeof currentFile)
+  
+  const fileParts = currentFile.split(`<section id="posts">`)
+  const startOfFile = fileParts[0]
+  const secondPartOfFile = fileParts[1].split("</section>")[1]
+  const fileContents = startOfFile + linkList + secondPartOfFile;
+  const fd = openSync(homeFilePath, 'w+')
+  if(currentFile === fileContents) {
+    close(fd);
+  } else{
+    writeFileSync(fd, fileContents);
+    close(fd);
+  }
+
 }
 
 try {
@@ -155,6 +196,9 @@ try {
 
   // file generation
   generatePostFiles(postPaths)
+
+  // home generation
+  generateHomePage(postPaths)
 } catch (err) {
   console.error(err);
 }
