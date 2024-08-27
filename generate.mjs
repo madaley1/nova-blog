8d9ab0e44c1b506d7980e9f8eb180dee3d56241b
madaley1 <mor2daley@gmail.com>
2024-08-27 06:58:49 -0700

    cleanup

--- generate.mjs
+++ generate.mjs
@@ -6,7 +6,9 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const folderName = join(__dirname, "posts")
+const postsFolder = join(__dirname, "posts")
+
+const converter = new showdown.Converter();
 
 const getDirectories = (source) =>
   readdirSync(source, { withFileTypes: true })
@@ -18,12 +20,11 @@ const getFiles = (source) =>
     .filter((dirent) => statSync(dirent.path + "/" + dirent.name).isFile() && dirent.name.split('.')[1] !== "html")
     .map(dirent => dirent.path + "/" + dirent.name.split('.').slice(0, -1).join(""));
 
-const converter = new showdown.Converter();
 
 const translateMdToHTML = (source) => {
-  if(!existsSync(source)) throw new Error('file does not exist')
+  if(!existsSync(source)) throw new Error('file does not exist');
   const data = readFileSync(source, "utf8");
-  return converter.makeHtml(data)
+  return converter.makeHtml(data);
 }
 
 const capitalizeFirstLetter = (string) => {
@@ -36,10 +37,11 @@ const navHTML = `
     <a href="/about">About</a>
     <a href="/posts">Posts</a>
   </nav>
-`
+`;
 
 const generatePostsPage = (postPaths) => {
-    let linkList = ''
+    let linkList = '';
+    // generate post links
     for(let i = postPaths.length; i > 0; i--){
       const postPath = postPaths[i - 1];
 
@@ -47,35 +49,43 @@ const generatePostsPage = (postPaths) => {
       
       const postName = splitPostPath[splitPostPath.length - 1];
       const postDate = splitPostPath[splitPostPath.length - 2];
-      console.log(postDate)
       linkList += `
         <div class="postLink">
           <a href="/${relative('.', postPath)}.html">${capitalizeFirstLetter(postName)} | ${postDate}</a>
         </div>
-      `
+      `;
     }
 
-    const bodyOfFile = `<section id="posts">${linkList}</section>`;
+    // setup
+    const postsFilePath = join(__dirname, "/posts/index.html");
+    const currentFile = readFileSync(postsFilePath, { encoding: 'utf8', flag: 'r' });
 
-    const postsFilePath = join(__dirname, "/posts/index.html")
-    const currentFile = readFileSync(postsFilePath, { encoding: 'utf8', flag: 'r' })
-    const fileParts = currentFile.split(`<section id="posts">`)
-    const startOfFile = fileParts[0]
-    const secondPartOfFile = fileParts[1].split("</section>")[1]
+    // parts construction
+    const bodyOfFile = `<section id="posts">${linkList}</section>`;
+    const fileParts = currentFile.split(`<section id="posts">`);
+    const startOfFile = fileParts[0];
+    const secondPartOfFile = fileParts[1].split("</section>")[1];
+
+    // file construction
     const fileContents = startOfFile + bodyOfFile  + secondPartOfFile;
+
+    //
     if(currentFile !== fileContents) {;
-      const fd = openSync(postsFilePath, 'w+')
+      const fd = openSync(postsFilePath, 'w+');
       writeFileSync(fd, fileContents);
       close(fd);
     } 
-
 }
 
 const generatePostFiles = (postPaths) => {
+  // post generation
   for(const postPath of postPaths){
+    // setup
     const mdFile = postPath + ".md";
-    const splitPath = postPath.split('/')
-    const postTitle = splitPath[splitPath.length -1]
+    const splitPath = postPath.split('/');
+    const postTitle = splitPath[splitPath.length -1];
+
+    // building parts
     const postHTML = translateMdToHTML(mdFile);
     const beginningOfFile = `
       <!DOCTYPE html>
@@ -102,19 +112,20 @@ const generatePostFiles = (postPaths) => {
 
     const fileContents = beginningOfFile + bodyOfFile + endOfFile;
 
-    const fd = openSync(postPath+".html", 'w+')
+    // regeneration
     const currentFile = readFileSync(fd, { encoding: 'utf8', flag: 'r' });
-    if(currentFile === fileContents) {
-      close(fd);
-    } else{
+    if(currentFile !== fileContents) {
+      const fd = openSync(postPath+".html", 'w+');
       writeFileSync(fd, fileContents);
-      close(fd);
+      close(fd);    
     }
   }
 }
 
 const generateHomePage = (postPaths) => {
-  let linkList = ''
+  let linkList = '<section id="posts"><h2>Recent Posts</h2>';
+
+  // generating post list
   for(let i = postPaths.length; i - 1 > -1 && i > postPaths.length - 5; i--){
     const postPath = postPaths[i - 1];
 
@@ -127,18 +138,23 @@ const generateHomePage = (postPaths) => {
       <div class="postLink">
         <a href="/${relative('.', postPath)}.html">${capitalizeFirstLetter(postName)} | ${postDate}</a>
       </div>
-    `
+    `;
   }
+  linkList += `</section>`;
 
-  const homeFilePath = join(__dirname, "/index.html")
+  // construction setup
+  const homeFilePath = join(__dirname, "/index.html");
   const currentFile = readFileSync(homeFilePath, { encoding: 'utf8', flag: 'r' });
   
-  const fileParts = currentFile.split(`<section id="posts"><h2>Recent Posts</h2>`)
-  const startOfFile = fileParts[0]
-  const secondPartOfFile = fileParts[1].split("</section>")[1]
-  const fileContents = startOfFile + `<section id="posts"><h2>Recent Posts</h2>` + linkList +  `</section>` + secondPartOfFile;
+  // file construction
+  const fileParts = currentFile.split(`<section id="posts"><h2>Recent Posts</h2>`);
+  const startOfFile = fileParts[0];
+  const secondPartOfFile = fileParts[1].split("</section>")[1];
+  const fileContents = startOfFile + linkList  + secondPartOfFile;
+
+  // regeneration
   if(currentFile !== fileContents) {;
-    const fd = openSync(homeFilePath, 'w+')
+    const fd = openSync(homeFilePath, 'w+');
     writeFileSync(fd, fileContents);
     close(fd);
   } 
@@ -146,24 +162,30 @@ const generateHomePage = (postPaths) => {
 }
 
 try {
-  if(!existsSync(folderName)){
-    mkdir(folderName)
+  // ensure posts folder exists
+  if(!existsSync(postsFolder)){
+    mkdir(postsFolder);
   }
-  const directories = getDirectories(folderName);
-  const postPaths = []
+
+  // get all posts
+  const directories = getDirectories(postsFolder);
+  const postPaths = [];
+
+  // generate filepaths of posts
   for(const dir of directories){
-    const dirSource = join(__dirname, `posts/${dir}`)
+    const dirSource = join(__dirname, `posts/${dir}`);
     const fileArray = getFiles(dirSource);
-    fileArray.forEach((filePath) => postPaths.push(filePath))
+    fileArray.forEach((filePath) => postPaths.push(filePath));
   }
+
   // posts page generation
-  generatePostsPage(postPaths)
+  generatePostsPage(postPaths);
 
   // file generation
-  generatePostFiles(postPaths)
+  generatePostFiles(postPaths);
 
   // home generation
-  generateHomePage(postPaths)
+  generateHomePage(postPaths);
 } catch (err) {
   console.error(err);
 }
